49935a0ac1b323e71af2c84604bb502fd5c3fe22
AlexRGB <alexismtz200326@gmail.com>
2024-04-25 03:01:13 -0600

    Cambio mensaje

--- src/database.js
+++ src/database.js
@@ -1,24 +1,24 @@
-const { Pool } = require('pg'); 
+const { Pool } = require("pg");
 
-const pool= new Pool({
-    user: 'marisolnunez', 
-    host: 'localhost',
-    database: 'restaurante',
-    password: '123', 
-    port: '5432'
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "restaurante",
+  password: "linux",
+  port: "5432",
 });
 pool.connect((err, client, done) => {
-    if (err) {
-      console.error('Error al conectar a la base de datos', err);
-    } else {
-      console.log('Conexión exitosa a la base de datos');
-      // Puedes realizar consultas aquí
-      // client.query('SELECT * FROM tu_tabla', (err, result) => {
-      //   console.log(result.rows);
-      //   done();
-      //   pool.end(); // Cierra la conexión después de realizar las consultas necesarias
-      // });
-    }
-  });
-  
-  module.exports = pool;
+  if (err) {
+    console.error("Error al conectar a la base de datos", err);
+  } else {
+    console.log("Conexión exitosa a la base de datos");
+    // Puedes realizar consultas aquí
+    // client.query('SELECT * FROM tu_tabla', (err, result) => {
+    //   console.log(result.rows);
+    //   done();
+    //   pool.end(); // Cierra la conexión después de realizar las consultas necesarias
+    // });
+  }
+});
+
+module.exports = pool;
